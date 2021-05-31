8fff8da0334c55eb74ebba846ec98061e5cccfe9
omertastan <otastanfanus@gmail.com>
2021-05-31 23:39:47 +0300

    a

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Router, Route, Switch } from 'react-router-dom';
